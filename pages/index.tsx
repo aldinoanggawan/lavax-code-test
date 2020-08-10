075483caae748974edb9fb39051b436626a29d34
Aldino Dwi Anggawan <aldinoanggawan@gmail.com>
2020-08-10 23:00:34 +0800

    Initial commit. Configured typescript and styled-components.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/layout'
+
+import { H1 } from '../styles/content'
+
+const Home = () => {
+  return (
+    <Layout title='| Home'>
+      <H1>Hello World !</H1>
+    </Layout>
+  )
+}
+
+export default Home
