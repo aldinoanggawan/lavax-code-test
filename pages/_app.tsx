9c5550a5e47a8cb070eb66a2decb84c2fa065ee8
Aldino Dwi Anggawan <aldinoanggawan@gmail.com>
2020-08-11 01:35:18 +0800

    Configured apollo (client & server). Added ts rule.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
+
+import { useApollo } from '../apollo/client'
 import GlobalStyle from '../styles/globalStyle'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import Layout from '../components/layout'
+import { gql, useQuery } from '@apollo/client'
 
+import Layout from '../components/layout'
 import { H1 } from '../styles/content'
 
+const GetHello = gql`
+  query GetHello {
+    hello
+  }
+`
+
 const Home = () => {
+  const { data, error, loading } = useQuery(GetHello)
+
   return (
     <Layout title='| Home'>
-      <H1>Hello World !</H1>
+      {loading && <H1>Loading...</H1>}
+      {error && <H1>Uh oh an error occured :(</H1>}
+      {data && <H1>{data.hello}</H1>}
     </Layout>
   )
 }
