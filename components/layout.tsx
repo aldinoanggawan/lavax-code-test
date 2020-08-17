ca5b2d785ef7cd82d7c7bd23dab169a8dc1bc9fa
Aldino Dwi Anggawan <aldinoanggawan@gmail.com>
2020-08-17 09:53:01 +0800

    Added PWA.

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,6 +13,7 @@ const Layout = ({ children, title }: Props) => (
       <title>LavaNote {title}</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link href='/manifest.json' rel='manifest'></link>
       <link
         href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap'
         rel='stylesheet'
